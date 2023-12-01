8f39af8c75a36aaab06ab21cf998733efee6d96c
Vanonym <60972074+brayevalerien@users.noreply.github.com>
2023-12-01 12:26:19 +0100

    solved day 1 pb 1

--- README.md
+++ README.md
@@ -1,5 +1,20 @@
 # AOC-2023
-My solution for the Advent of Code 2023, solved in V
+My solution for the Advent of Code 2023, solved in [V](https://github.com/vlang/v)!
+
+Here is my progress so far:
+![progress bar](https://geps.dev/progress/2)
+
+## How to run
+If you want to run the program for a specific day "XX" and a problem "Y" (1 or 2), use:
+```bash
+v run ./dayXX/pbY/
+```
+
+## Content
+This repository contain one directory per day and each day contains two directories: one for each problem of the day (./dayXX/pbY/ for problem Y of day XX). For each problem, there are three files:
+1. solution.v: the program to solve the problem
+2. test.txt: test data that is usually provided in the problem statement
+3. input.txt: input data
 
 ## Useful links
 - [The V langage](https://github.com/vlang/v)
---
+++ day01/pb1/solution.v
@@ -0,0 +1,37 @@
+module main
+
+import os
+
+fn main() {
+	// input_path := "./day01/pb1/test.txt"
+	input_path := "./day01/pb1/input.txt"
+
+	lines := os.read_lines(input_path) or { panic('Could not read input file.') }
+	mut sum := 0 // will contain the final result
+	for line in lines {
+		line_nums := get_line_nums(line)
+		sum += get_line_number(line_nums)
+	}
+	println("Final result: ${sum}")
+}
+
+// Given a string, returns the array of all numbers in this string
+fn get_line_nums(line string) []rune {
+	nums := [`0`, `1`, `2`, `3`, `4`, `5`, `6`, `7`, `8`, `9`]
+	mut line_nums := []rune{}
+	for c in line {
+		if rune(c) in nums {
+			line_nums << rune(c)
+		}
+	}
+	return line_nums
+}
+
+// Given a array of digits as runes, returns the 2-digits number formed by
+// the first digit in the array, concatenated with the last digit in the array.
+fn get_line_number(nums []rune) int {
+	// runes cannot be concatenated easily. So first cast to string, concatenate and cast to int.
+	mut res := rune(nums[0]).str()
+	res += rune(nums[nums.len-1]).str()
+	return res.int()
+}
